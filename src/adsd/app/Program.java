58efa0524a8318faed3b207d9d78be4653c6c06e
joelsoedito <49315324+joelsoedito@users.noreply.github.com>
2020-12-17 16:54:38 +0100

    Zoekfunctie in console variant toegevoegd. Nog niet af.

--- src/adsd/app/Program.java
+++ src/adsd/app/Program.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Program extends Application
 {
@@ -51,6 +52,50 @@ public class Program extends Application
 
         Locale.setDefault(new Locale("nl", "NL"));
         ResourceBundle rb = ResourceBundle.getBundle("lang");
+
+
+
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Voer vertrekplaats in: ");
+        String vertrekplaats = input.nextLine();
+
+//        System.out.println("Voer aankomstplaats in: ");
+//        String aankomstplaats = input.nextLine();
+
+        System.out.println("Welk vervoersmiddel?");
+        String vehicle = input.nextLine();
+
+
+        System.out.println("Hoelaat wilt u dat doen dan?");
+        Double tijd = input.nextDouble();
+        String tijd2 = String.valueOf(tijd);
+
+
+
+
+//        functie die zoekt op vertrek plaats
+        for (int i = 0; i < datahandler.getTripList().size(); i++ )
+        {
+            if (datahandler.getTrip(i).getLocationFrom().toLowerCase().contains(vertrekplaats.toLowerCase()))
+            {
+                for (int k = 0; k < datahandler.getTrip(i).getTripTimesList().size(); k++)
+                {
+                    if (datahandler.getTrip(i).getTripTimesList().get(k).getVehicleType().toLowerCase().contains(vehicle.toLowerCase()) &&
+                        String.valueOf(datahandler.getTrip(i).getTripTimesList().get(k).getDepTime()).contains(tijd2)  )  {
+                        System.out.println(i + ", " + k);
+
+
+                    }
+                }
+
+            }
+        }
+
+
+
+
 
         // Datahandler writeToJSON always at the end of the application
         datahandler.writeToExternalData();
--- src/adsd/app/Trip.java
+++ src/adsd/app/Trip.java
@@ -175,4 +175,6 @@ public class Trip
     {
         return times;
     }
+
+
 }
